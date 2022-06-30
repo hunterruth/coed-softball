827ddfca0bda3c4c24e4ecffc9d77302bf5512e4
Hunter Ruth <hunter@stitchdesignco.com>
2022-06-30 07:30:20 -0500

    build folder

---
+++ build/precache-manifest.271d32330a5d126632fb44adbb05fcc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcbd2833171d3a1ecbb5",
+    "url": "/softball/static/css/main.0125d656.chunk.css"
+  },
+  {
+    "revision": "bcbd2833171d3a1ecbb5",
+    "url": "/softball/static/js/main.bcbd2833.chunk.js"
+  },
+  {
+    "revision": "dc4dc0021066ace78671",
+    "url": "/softball/static/js/1.dc4dc002.chunk.js"
+  },
+  {
+    "revision": "68839f72ce1a32b2557a",
+    "url": "/softball/static/js/2.68839f72.chunk.js"
+  },
+  {
+    "revision": "79b3fc8af3f871d16ba2",
+    "url": "/softball/static/js/runtime~main.79b3fc8a.js"
+  },
+  {
+    "revision": "d35da2b18fe8e534c8d4259b41554875",
+    "url": "/softball/static/media/unathletics.d35da2b1.svg"
+  },
+  {
+    "revision": "78d2f3ec6b8002cdd3affeb7176c1776",
+    "url": "/softball/index.html"
+  }
+];
